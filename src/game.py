6660a2180ec364787540163afb4b0ce81cd65687
Diana <kyra169090@gmail.com>
2024-08-30 20:03:17 +0200

    adding changing cursor when hovering certain areas

--- src/game.py
+++ src/game.py
@@ -7,12 +7,13 @@ from inventory import Inventory
 class Game:
     def __init__(self):
         pygame.init()
-        self.WIDTH = 1500
-        self.HEIGHT = 1024
 
         # Screen setup
+        self.WIDTH = 1500
+        self.HEIGHT = 1024
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption('Deathdream')
+
         # Music
         pygame.mixer.init()
         pygame.mixer.music.load('../assets/sounds/background_music.mp3')
@@ -26,8 +27,12 @@ class Game:
     def run(self):
         running = True
         selected_item = None
-
+        hand_cursor = pygame.image.load('../assets/images/other/pointer.png')
+        
         while running:
+            moving_mouse_pos = pygame.mouse.get_pos()
+            hover = False
+            
             # The Pygame event loop (for a point&click mousedown is enough)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -60,6 +65,23 @@ class Game:
                             self.current_scene = box.next_scene
                             break
 
+            for box in self.current_scene.boxes:
+                if pygame.Rect(box.x, box.y, box.width, box.height).collidepoint(moving_mouse_pos):
+                    hover = True
+                    break
+
+            # Render the current scene first
             self.current_scene.render(self.screen, self.inventory)
+
+            if hover:
+                pygame.mouse.set_visible(False)
+                # Draw the custom cursor image at the mouse position after everything else
+                self.screen.blit(hand_cursor, (moving_mouse_pos[0] - hand_cursor.get_width() // 2, 
+                                               moving_mouse_pos[1] - hand_cursor.get_height() // 2))
+            else:
+                # Show the default system cursor
+                pygame.mouse.set_visible(True)
+
+            pygame.display.flip()
 
         pygame.quit()
